fe3111091ac81fee93f79832c196ea73a094412d
purvimisal <purvivmisal@gmail.com>
2019-11-03 12:45:35 -0800

    frontend

---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class EditExercise extends Component {
+  render() {
+    return (
+      <div>
+        <p>
+          EditExercise
+        </p>
+      </div>
+    )
+  }
+}
---
+++ src/components/exercises-list.component.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class ExercisesList extends Component {
+  render() {
+    return (
+      <div>
+        <p>
+          ExercisesList
+        </p>
+      </div>
+    )
+  }
+}
